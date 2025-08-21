27c0134faf7f97f38398925682119a32a82aabf1
PoJiC69 <cawancorp@gmail.com>
2025-08-22 02:45:38 +0700

    Update train_ppo.py

--- scripts/train_ppo.py
+++ scripts/train_ppo.py
@@ -1,3 +1,6 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import gymnasium as gym
 from stable_baselines3 import PPO
 from blender_env.BlenderGymEnv import BlenderGymEnv
